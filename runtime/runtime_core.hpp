5c0bd83e5e398615bc5e88ac6e011bd9ec2e5a45
NeoWise69 <64774463+NeoWise69@users.noreply.github.com>
2023-05-15 01:30:28 +1000

    started work on runtime lib

---
+++ runtime/runtime_core.hpp
@@ -0,0 +1,24 @@
+
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * This file is part of The WhiteGear Studio software property.
+ * If you get unexpected access to this file, or part ot whole codebase, you should
+ * report this source code leak and delete all copies of source code from all your machines.
+ ******************************************************************************/
+
+#ifndef WHITEGEAR_RUNTIME_CORE_HPP
+#define WHITEGEAR_RUNTIME_CORE_HPP
+
+#include <runtime/runtime_module.hpp>
+
+namespace wg {
+    class runtime_core {
+    public:
+        inline static const auto MAX_MODULES = 32;
+
+    private:
+        
+    };
+}
+
+#endif //WHITEGEAR_RUNTIME_CORE_HPP
