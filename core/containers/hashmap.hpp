5c85adf2d196322d34c1dbdc4ca549bc0c9e73e9
NeoWise69 <64774463+NeoWise69@users.noreply.github.com>
2023-05-22 18:19:55 +1000

    added hashmap (from std::umap)

---
+++ core/containers/hashmap.hpp
@@ -0,0 +1,50 @@
+
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * This file is part of The WhiteGear Studio software property.
+ * If you get unexpected access to this file, or part ot whole codebase, you should
+ * report this source code leak and delete all copies of source code from all your machines.
+ ******************************************************************************/
+
+#ifndef WHITEGEAR_HASHMAP_HPP
+#define WHITEGEAR_HASHMAP_HPP
+
+#include <unordered_map>
+#include "string.hpp"
+#include "string_view.hpp"
+#include "bounded_array.hpp"
+#include "array_view.hpp"
+
+namespace wg {
+    template<class KeyType, class ValueType>
+    using hashmap = std::unordered_map<KeyType, ValueType>;
+}
+
+namespace std {
+    template<>
+    struct hash<wg::string> {
+        inline size_t operator()(const wg::string& s) const noexcept {
+            return std::_Hash_array_representation(s.c_str(), s.size());
+        }
+    };
+    template<>
+    struct hash<wg::string_view> {
+        inline size_t operator()(const wg::string_view& s) const noexcept {
+            return std::_Hash_array_representation(s.c_str(), s.size());
+        }
+    };
+    template<class T, size_t N>
+    struct hash<wg::bounded_array<T, N>> {
+        inline size_t operator()(const wg::bounded_array<T, N>& a) const noexcept {
+            return std::_Hash_array_representation(a.data(), a.size());
+        }
+    };
+    template<class T>
+    struct hash<wg::array_view<T>> {
+        inline size_t operator()(const wg::array_view<T>& a) const noexcept {
+            return std::_Hash_array_representation(a.data(), a.size());
+        }
+    };
+}
+
+#endif //WHITEGEAR_HASHMAP_HPP
