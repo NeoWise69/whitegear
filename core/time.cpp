80877aae7dfb50983f62196a8e9d8299cae77d28
NeoWise69 <64774463+NeoWise69@users.noreply.github.com>
2023-06-02 12:38:32 +1000

    simple time utility

---
+++ core/time.cpp
@@ -0,0 +1,57 @@
+
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * This file is part of The WhiteGear Studio software property.
+ * If you get unexpected access to this file, or part ot whole codebase, you should
+ * report this source code leak and delete all copies of source code from all your machines.
+ ******************************************************************************/
+
+#include "time.hpp"
+
+#include <chrono>
+
+namespace wg {
+    double time_point::get(time_point::e_resolution resolution) const {
+        switch (resolution) {
+            case e_resolution::nanoseconds:     return (mTimeInNanoseconds);
+            case e_resolution::milliseconds:    return mTimeInNanoseconds * (0.000001);
+            case e_resolution::seconds:         return mTimeInNanoseconds * (1e-9);
+            case e_resolution::minutes:         return mTimeInNanoseconds * (1.66667e-11);
+            case e_resolution::hours:           return mTimeInNanoseconds * (2.778e-13);
+        }
+        return mTimeInNanoseconds;
+    }
+
+    time_point time_point::now() {
+        // should return if nanoseconds, since std::chrono::steady_clock works with ns.
+        return double(std::chrono::duration_cast<std::chrono::duration<double, std::nano>>(std::chrono::system_clock::now().time_since_epoch()).count());
+    }
+
+    time_point operator+(const time_point& a, const time_point& b) {
+        return a.get(time_point::nanoseconds) + b.get(time_point::nanoseconds);
+    }
+    time_point operator-(const time_point& a, const time_point& b) {
+        return a.get(time_point::nanoseconds) - b.get(time_point::nanoseconds);
+    }
+
+    time_point operator""_h(const long double t) {
+        return (double)(t * 3600000000000);
+    }
+
+    time_point operator""_m(const long double t) {
+        return (double)(t * 60000000000);
+    }
+
+    time_point operator""_s(const long double t) {
+        return (double)(t * 1000000000);
+    }
+
+    time_point operator""_ms(const long double t) {
+        return (double)(t * 1000000);
+    }
+
+    time_point operator""_ns(const long double t) {
+        return double(t);
+    }
+
+}
---
+++ core/time.hpp
@@ -0,0 +1,56 @@
+
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * This file is part of The WhiteGear Studio software property.
+ * If you get unexpected access to this file, or part ot whole codebase, you should
+ * report this source code leak and delete all copies of source code from all your machines.
+ ******************************************************************************/
+
+#ifndef WHITEGEAR_TIME_HPP
+#define WHITEGEAR_TIME_HPP
+
+#include <core/typedefs.hpp>
+
+namespace wg {
+    class time_point {
+    public:
+        inline time_point() = default;
+        inline ~time_point() = default;
+        inline time_point(double ns) : mTimeInNanoseconds(ns) {}
+
+        enum e_resolution : uint {
+            nanoseconds,
+            milliseconds,
+            seconds,
+            minutes,
+            hours
+        };
+        double get(e_resolution resolution) const;
+
+        static time_point now();
+    private:
+        double mTimeInNanoseconds = {};
+    };
+    time_point operator+(const time_point& a, const time_point& b);
+    time_point operator-(const time_point& a, const time_point& b);
+
+    time_point operator""_h(const long double t);
+    time_point operator""_m(const long double t);
+    time_point operator""_s(const long double t);
+    time_point operator""_ms(const long double t);
+    time_point operator""_ns(const long double t);
+
+    struct frame_time_stats {
+        // general
+        time_point frame_time;
+        time_point tick_time;
+        // frame related
+        time_point frame_begin_time;
+        time_point frame_end_time;
+
+        time_point world_tick_time;
+    };
+    inline static frame_time_stats GTimeStats;
+}
+
+#endif //WHITEGEAR_TIME_HPP
